d30c121894db3ae2cdd5803e8fed8485967367e8
adnan <adnan.nassar90@gmail.com>
2024-01-14 21:33:14 +0100

    added: swagger integration

--- backend/src/main/java/nassar/goldagram/backend/configs/SwaggerConfig.java
+++ backend/src/main/java/nassar/goldagram/backend/configs/SwaggerConfig.java
@@ -7,11 +7,12 @@ import org.springframework.context.annotation.Configuration;
 
 @Configuration
 public class SwaggerConfig {
+
     @Bean
-    public GroupedOpenApi publicApi() {
+    public GroupedOpenApi customApi() {
         return GroupedOpenApi.builder()
-                .group("springdoc")
-                .pathsToMatch("/**")
+                .group("custom-api")
+                .packagesToScan("nassar.goldagram.backend.controllers")
                 .build();
     }
 }
